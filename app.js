d3a6bd8bf53a8e1cf8867a8e5ea444ba121e43f7
Etienne <bissonnette@live.ca>
2022-09-18 18:13:01 -0400

    Fixed individual blog post page with MongoDB and added compose in navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 require("dotenv").config()
+const date = new Date()
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -28,16 +29,20 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.listen(process.env.PORT, function () {
+  console.log("Server started on port " + process.env.PORT);
+});
+
 
 // GET
 
 app.get("/", function (req, res) {
 
   Post.find({}, (e, postList) => {
-    res.render('home',{
+    res.render('home', {
       homeStart: homeStartingContent,
       posts: postList,
-      _: _ 
+      _: _
     })
   })
 })
@@ -55,24 +60,23 @@ app.get("/contact", function (req, res) {
 })
 
 app.get("/compose", function (req, res) {
-  res.render('compose');
+  res.render('compose', {
+    date: date.toLocaleDateString()
+  });
 })
-
 
 
 app.get("/post/:postTitle", function (req, res) {
 
-  const requestedPost = req.params.postTitle;
+  const requestedPostID = req.params._id;
 
-  postList.forEach(post => {
-    if (_.lowerCase(post.title) === _.lowerCase(requestedPost)) {
-
-      res.render("post", {
-        title: post.title,
-        body: post.body
-      })
-      res.redirect("/post");
-    }
+  Post.findOne({
+    id: requestedPostID
+  }, (e, post) => {
+    res.render("post", {
+      title: post.title,
+      body: post.body
+    })
   })
 })
 
@@ -87,9 +91,3 @@ app.post("/compose", function (req, res) {
 
   res.redirect("/");
 })
-
-
-
-app.listen(process.env.PORT, function () {
-  console.log("Server started on port " + process.env.PORT);
-});
